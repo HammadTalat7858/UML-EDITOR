4cfb11b843a9695beb3330d3f98e40924ec442e3
HammadTalat7858 <l227858@lhr.nu.edu.pk>
2024-11-17 13:33:01 +0500

    Added Support in module-info.java for Exporting Images as JPEG and PNG

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module com.example.pscd {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
+    requires java.desktop;
 
     opens com.example.pscd to javafx.fxml;
     exports com.example.pscd;
