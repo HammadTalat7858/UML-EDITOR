b35e51ae0d976d74842261a1ef8f8711f733fca0
MoeezAzeem <124194519+MoeezAzeem@users.noreply.github.com>
2024-12-06 16:13:31 +0500

    Prepared the Main Class for Javadoc Documentation.

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,6 +1,5 @@
 package com.example;
 
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -8,7 +7,25 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * The {@code Main} class serves as the entry point for the UML Editor application.
+ * <p>
+ * It initializes and displays the JavaFX application, setting up the primary stage
+ * with the user interface defined in the {@code class_diagram.fxml and @code use_case.fxml} file.
+ * </p>
+ *
+ * @author Hammad Tallat
+ * @author Ahmed Moeez
+ * @author Muhammad Hassnain
+ */
 public class Main extends Application {
+
+    /**
+     * Starts the JavaFX application by loading the primary stage and its scene.
+     *
+     * @param stage the primary stage for this JavaFX application
+     * @throws IOException if the FXML file cannot be loaded
+     */
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(com.example.ClassDiagram.Main.class.getResource("class_diagram.fxml"));
@@ -18,9 +35,13 @@ public class Main extends Application {
         stage.setScene(scene);
         stage.show();
     }
+
+    /**
+     * The main method that launches the JavaFX application.
+     *
+     * @param args command-line arguments (not used in this application)
+     */
     public static void main(String[] args) {
         launch();
     }
-
-
 }
