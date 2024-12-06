7f3ed23a453b3844b103d62ae66deecc26c57f9e
HammadTalat7858 <l227858@lhr.nu.edu.pk>
2024-12-06 17:36:08 +0500

    Created Java Docs VIA Terminal

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.example.ClassDiagram","l":"Controller"},{"p":"com.example.ClassDiagram","l":"Controller.LineConnection"}];updateSearchResults();
