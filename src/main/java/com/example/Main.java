dd7aebe459ec126580ddd3697d3cb5cc3fddfc57
HammadTalat7858 <l227858@lhr.nu.edu.pk>
2024-12-10 18:43:13 +0500

    Added Icons Path in Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -6,8 +6,8 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Objects;
 
 /**
  * The {@code Main} class serves as the entry point for the UML Editor application.
@@ -31,13 +31,14 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(com.example.ClassDiagram.Main.class.getResource("class_diagram.fxml"));
-        stage.getIcons().add(new Image(new FileInputStream("uml_icon_circular-1.png")));
-        stage.getIcons().add(new Image(new FileInputStream("uml_icon_circular-2.png")));
-        stage.getIcons().add(new Image(new FileInputStream("uml_icon_circular-3.png")));
-        stage.getIcons().add(new Image(new FileInputStream("uml_icon_circular-4.png")));
-        stage.getIcons().add(new Image(new FileInputStream("uml_icon_circular-5.png")));
-        stage.getIcons().add(new Image(new FileInputStream("uml_icon_circular-6.png")));
-        stage.getIcons().add(new Image(new FileInputStream("uml_icon_circular-7.png")));
+        stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/com/example/Icons/uml_icon_circular-1.png"))));
+        stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/com/example/Icons/uml_icon_circular-2.png"))));
+        stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/com/example/Icons/uml_icon_circular-3.png"))));
+        stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/com/example/Icons/uml_icon_circular-4.png"))));
+        stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/com/example/Icons/uml_icon_circular-5.png"))));
+        stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/com/example/Icons/uml_icon_circular-6.png"))));
+        stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/com/example/Icons/uml_icon_circular-7.png"))));
+
 
         Scene scene = new Scene(fxmlLoader.load());
 
